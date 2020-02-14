dd6ce70375ff6aba9e8d58b086764a3b086245c0
Ubuntu <ubuntu@ip-172-31-16-189.us-west-2.compute.internal>
2020-02-14 01:08:18 +0000

    First commit

--- src/App.js
+++ src/App.js
@@ -6,18 +6,22 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+	<h1>taylorgriffin.io</h1>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h3>
+          Site in progress...
+        </h3>
+	<p>
+	  In the meantime, feel free to check out my&nbsp;
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/taylorrgriffin"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          GitHub
         </a>
+	</p>
       </header>
     </div>
   );
